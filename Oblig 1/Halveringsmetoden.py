80c7625b015ad532c9461247d1b495021493dd54
Martin Hegnum Johannessen <martinhejo@hotmail.no>
2023-08-27 16:03:15 +0200

    Add: Halveringsmetoden

---
+++ Oblig 1/Halveringsmetoden.py
@@ -0,0 +1,21 @@
+import numpy as np
+def bisection(f, a, b, eps):
+    it = 0
+    nr = 0 #maximum iterations allowed
+    #if f(a) * f(b) > 0:
+    if np.sign(f(a))*np.sign(f(b)) > 0:
+        return " "
+    while (b-a)/2 > eps:
+        mid = (a+b)/2
+        if f(mid) == 0:
+            break
+        if f(a) * f(mid) < 0:
+            b = mid
+        else:
+            a = mid
+            if it < nr:
+                pass
+            else:
+                return mid
+            it += 1
+            return mid
