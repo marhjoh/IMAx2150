c0e0f7713222203fdb0f9d9504d79dcceb4500f5
martinhejo <marhjoh@stud.ntnu.no>
2023-08-31 11:38:08 +0200

    Add: Oppgave 2

---
+++ Oblig 2/Oppgave 2.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def fikspunkt_solve(g, x0, tol):
+    x_i = x0
+    max_iterations = 1000  # Sett øvre grense for antall iterasjoner
+
+    for i in range(max_iterations):
+        x_i = g(x_i)
+        if abs(x_i - x0) < tol:
+            return x_i
+        x0 = x_i
+
+    return x_i
+
+f1 = lambda x: x ** 3 - 2 * x + 2
+f2 = lambda x: np.exp(x) + x - 7
+f3 = lambda x: np.exp(x) + np.sin(x) - 4
+
+startverdier = [1.5, 1.5, 1.0]  # Passende startverdier for hver funksjon
+funcs = [f1, f2, f3]
+maks_feil = 1e-8
+sols = []
+
+for func, x0 in zip(funcs, startverdier):           # Kombiner elementene fra listene
+    x_sol = fikspunkt_solve(func, x0, maks_feil)  # Bruk riktig funksjon og startverdi x0
+    sols.append(x_sol)
+
+print(f"Løsning til første ligning x = {sols[0]}")
+print(f"Løsning til andre ligning x = {sols[1]}")
+print(f"Løsning til tredje ligning x = {sols[2]}")
